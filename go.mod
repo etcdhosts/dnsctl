41884d05de1c694ead1fd35a1d9ad8cf47d2a3c8
mritd <mritd@linux.com>
2020-12-17 18:12:51 +0800

    feat(cmd): add dump cmd
    add dump cmd
    
    Signed-off-by: mritd <mritd@linux.com>

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/etcdhosts/dnsctl
 go 1.15
 
 require (
-	github.com/etcdhosts/client-go v0.0.0-20201217090825-a13486de2476
+	github.com/etcdhosts/client-go v0.0.0-20201217100945-4964f879de0f
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mritd/logger v0.0.3
 	github.com/urfave/cli/v2 v2.3.0
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
 	"os"
 	"path/filepath"
@@ -61,6 +62,7 @@ func main() {
 			exampleCmd(),
 			addCmd(),
 			delCmd(),
+			dumpCmd(),
 		},
 	}
 	err = app.Run(os.Args)
@@ -152,6 +154,38 @@ func delCmd() *cli.Command {
 
 			logger.Info("host delete success.")
 			return nil
+		},
+	}
+}
+
+func dumpCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "dump",
+		Usage: "dump all DNS records",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "out",
+				Aliases: []string{"o"},
+				Value:   "",
+				Usage:   "dump file storage location",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			hc, err := createClient(c)
+			if err != nil {
+				return err
+			}
+
+			hf, err := hc.ReadHosts()
+			if err != nil {
+				return err
+			}
+			if c.String("out") == "" {
+				fmt.Printf("\n%s", hf.String())
+				return nil
+			} else {
+				return ioutil.WriteFile(c.String("out"), []byte(hf.String()), 0644)
+			}
 		},
 	}
 }
