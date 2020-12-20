2e6183dcf7fb40df364c3d5cff2875f5febbd51a
mritd <mritd@linux.com>
2020-12-20 23:09:21 +0800

    feat(cmd): add edit
    add edit
    
    Signed-off-by: mritd <mritd@linux.com>

--- cmd_edit.go
+++ cmd_edit.go
@@ -2,6 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/mritd/logger"
+
+	"github.com/etcdhosts/client-go"
 
 	"github.com/urfave/cli/v2"
 )
@@ -11,7 +19,63 @@ func editCmd() *cli.Command {
 		Name:  "edit",
 		Usage: "Edit the hosts file with an editor",
 		Action: func(c *cli.Context) error {
-			return fmt.Errorf("the edit command has not yet been implemented")
+
+			f, err := ioutil.TempFile("", "dnsctl")
+			if err != nil {
+				return err
+			}
+			defer func() {
+				_ = f.Close()
+				_ = os.Remove(f.Name())
+			}()
+
+			hc, err := createClient(c)
+			if err != nil {
+				return err
+			}
+
+			hf, err := hc.ReadHostsFile()
+			if err != nil {
+				return err
+			}
+			_, err = fmt.Fprint(f, hf.String())
+			if err != nil {
+				return err
+			}
+
+			// get os editor
+			editor := "vim"
+			if runtime.GOOS == "windows" {
+				editor = "notepad"
+			}
+			if v := os.Getenv("VISUAL"); v != "" {
+				editor = v
+			} else if e := os.Getenv("EDITOR"); e != "" {
+				editor = e
+			}
+
+			cmd := exec.Command(editor, f.Name())
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err = cmd.Run()
+			if err != nil {
+				return err
+			}
+
+			nf, err := os.Open(f.Name())
+			if err != nil {
+				return err
+			}
+			defer func() { _ = nf.Close() }()
+			hm := client.Parse2Map(nf)
+			if hm.String() == hf.String() {
+				logger.Info("DNS records not change.")
+				return nil
+			} else {
+				logger.Info("DNS records updated.")
+				return hc.ForcePutHostsFile(f)
+			}
 		},
 	}
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/charmbracelet/bubbletea v0.12.2
-	github.com/etcdhosts/client-go v0.0.0-20201217111448-e9fcdc1a701f
+	github.com/etcdhosts/client-go v0.0.0-20201220145558-a5aefe345860
 	github.com/lucasb-eyer/go-colorful v1.0.3
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mritd/logger v0.0.3
