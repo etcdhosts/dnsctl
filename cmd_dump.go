5ff52cf1bda42933d869fa8bdff1c3dd8f531a1b
mritd <mritd@linux.com>
2021-03-01 10:18:15 +0800

    feat(dump): add revision
    add revision
    
    Signed-off-by: mritd <mritd@linux.com>

--- cmd_dump.go
+++ cmd_dump.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 
+	"github.com/etcdhosts/client-go"
+
 	"github.com/urfave/cli/v2"
 )
 
@@ -18,6 +20,12 @@ func dumpCmd() *cli.Command {
 				Value:   "",
 				Usage:   "dump file storage location",
 			},
+			&cli.Int64Flag{
+				Name:    "revision",
+				Aliases: []string{"r"},
+				Value:   0,
+				Usage:   "dump hosts file from etcd revision",
+			},
 		},
 		Action: func(c *cli.Context) error {
 			hc, err := createClient(c)
@@ -25,9 +33,17 @@ func dumpCmd() *cli.Command {
 				return err
 			}
 
-			hf, err := hc.ReadHostsFile()
-			if err != nil {
-				return err
+			var hf *client.HostsFile
+			if c.Int64("revision") > 0 {
+				hf, err = hc.ReadHostsFileByRevision(c.Int64("revision"))
+				if err != nil {
+					return err
+				}
+			} else {
+				hf, err = hc.ReadHostsFile()
+				if err != nil {
+					return err
+				}
 			}
 
 			if c.String("out") == "" {
